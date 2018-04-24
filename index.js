91b81d47a6d7f7a7b595311727c70f1fb090b45d
mythreyi <mythreyi.polsani@gmail.com>
2018-04-24 14:50:55 -0500

    topperf 1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 
 app.get('/topperformers', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
-    client.query('SELECT * FROM salesforce.account', function(err, result) {
+    client.query('SELECT * FROM salesforce.account,salesforce.user', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
